bf18d7fc2c622ae97d853af2050a578199d3a436
Cody Sork <codysork@tutanota.com>
2020-04-19 19:45:47 -0700

    Removed a bar; adjusted colors and styles

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -4,5 +4,4 @@ pkill polybar
 
 sleep 1;
 
-polybar --reload top -c ~/.config/polybar/config_top.ini &
-polybar --reload bottom -c ~/.config/polybar/config_bottom.ini &
+polybar --reload top -c ~/.config/polybar/config.ini &

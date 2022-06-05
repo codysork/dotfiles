cc6d45e85ee1be9be6caace86885fd394d70d069
Cody Sork <codysork@pm.me>
2022-06-05 15:38:16 -0700

    Fixed launch script

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/env sh
 
 if [[ $(ps aux | pgrep polybar) ]]; then
-  killall polybar && killall polybar && killall polybar && \
-  polybar --reload top -c ~/.config/polybar/config.ini &
+  killall polybar && killall polybar && \
+  polybar --reload top -c ~/.config/polybar/config.ini && \
   polybar --reload bottom -c ~/.config/polybar/config.ini &
 else
-  polybar --reload top -c ~/.config/polybar/config.ini &
+  polybar --reload top -c ~/.config/polybar/config.ini &&
   polybar --reload bottom -c ~/.config/polybar/config.ini &
 fi

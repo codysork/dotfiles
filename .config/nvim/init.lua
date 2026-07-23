5580f90e4e6d32563134a4f7c0c0131a20b83b83
Cody Sork <codysork@pm.me>
2026-07-22 17:01:05 -0700

    Added Telekasten hotkeys and moved to a new note vault

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,46 @@
+--------------------------------------------------------------------------
+--                            BOILERPLATE                               --
+--------------------------------------------------------------------------
+
 require("config.lazy")
 
--- Appearance
-vim.o.background = "dark" -- or "light" for light mode
+--------------------------------------------------------------------------
+--                             Appearance                               --
+--------------------------------------------------------------------------
+
+vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
--- Clipboard
+--------------------------------------------------------------------------
+--                              Defaults                                --
+--------------------------------------------------------------------------
+
+vim.g.mapleader = ","
 vim.opt.clipboard = "unnamedplus"
 
--- Telekasten
+--------------------------------------------------------------------------
+--                             Telekasten                               --
+--------------------------------------------------------------------------
+
 require('telekasten').setup({
-  home = vim.fn.expand("~/Sync/CodyVault"), -- Put the name of your notes directory here
+  -- Put the name of your notes directory here
+  home = vim.fn.expand("~/Sync/CodysNewVault"), 
 })
+
+-- Mneumonic: z for zettlekasten
+-- Launch panel if nothing is typed after <leader>z
+vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
+
+-- Most used functions
+vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")  -- b for backlinks
+vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")   -- c for calendar
+vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")      -- d for day
+vim.keymap.set("n", "<leader>ze", "<cmd>Telekasten follow_link<CR>")     -- e for explore
+vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")      -- f for find
+vim.keymap.set("n", "<leader>zi", "<cmd>Telekasten insert_img_link<CR>") -- I for insert image
+vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")        -- n for new note
+vim.keymap.set("n", "<leader>zs", "<cmd>Telekasten search_notes<CR>")    -- s for search
+
+-- Call insert link automatically when we start typing a link
+vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+
